1919bbef3599fa73f50cea80211578c7b126e63f
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-08-22 18:41:53 +0900

    035 문자열 출력

---
+++ 03. 파이썬 문자열/035.py
@@ -0,0 +1,8 @@
+# 변수에 다음과 같이 문자열과 정수가 바인딩되어 있을 때 % formatting을 사용해서 다음과 같이 출력해보세요.
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+
+print("이름: %s 나이: %d" % (name1, age1))
+print("이름: %s 나이: %d" % (name2, age2))
