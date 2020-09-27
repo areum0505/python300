bbd32560b8cc69be10be61970932a82228e94044
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-09-27 16:05:05 +0900

    219

---
+++ 09. 파이썬 함수/219.py
@@ -0,0 +1,10 @@
+# 두 개의 숫자를 입력받아 합/차/곱/나눗셈을 출력하는 print_arithmetic_operation 함수를 작성하라.
+
+def print_arithmetic_operation(a, b):
+    print(a, "+", b, "=", a + b)
+    print(a, "-", b, "=", a - b)
+    print(a, "*", b, "=", a * b)
+    print(a, "/", b, "=", a / b)
+
+
+print_arithmetic_operation(3, 4)
