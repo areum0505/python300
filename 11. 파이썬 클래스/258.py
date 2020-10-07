68d32c7d40f0876e7e3a472c1405e43d24f463cf
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-10-07 22:25:54 +0900

    258 클래스 메소드 - 2

---
+++ 11. 파이썬 클래스/258.py
@@ -0,0 +1,22 @@
+# 사람 (Human) 클래스에 (이름, 나이, 성별)을 받는 setInfo 메소드를 추가하세요.
+
+class Human:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def who(self):
+        print("이름: {} 나이: {} 성별: {}".format(self.name, self.age, self.gender))
+
+    def setInfo(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        
+        
+areum = Human("모름", 0, "모름")
+areum.who()    
+
+areum.setInfo("아름", 25, "여자")
+areum.who() 
