eca7a6b8299564c7b2677c139dbda1f760740603
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-10-08 22:44:16 +0900

    260 에러의 원인

---
+++ 11. 파이썬 클래스/260.py
@@ -0,0 +1,11 @@
+# 아래와 같은 에러가 발생한 원인에 대해 설명하세요.
+
+class OMG :
+    def print() :
+        print("Oh my god")
+
+myStock = OMG()
+myStock.print()
+
+# myStock.print()
+# TypeError: print() takes 0 positional arguments but 1 was given
