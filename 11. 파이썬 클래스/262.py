fcde9c6cbe962de426d9a7f92623908a98f9ddd9
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-10-08 22:46:04 +0900

    262 생성자

---
+++ 11. 파이썬 클래스/262.py
@@ -0,0 +1,9 @@
+# Stock 클래스의 객체가 생성될 때 종목명과 종목코드를 입력 받을 수 있도록 생성자를 정의해보세요.
+
+class Stock:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+
+삼성 = Stock("삼성전자", "005930")
