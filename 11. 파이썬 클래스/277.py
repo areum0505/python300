e2787b8a4e98c9e822ee5a630e3b6400b8ee57e3
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-10-09 23:02:46 +0900

    277 이자 지급하기

---
+++ 11. 파이썬 클래스/277.py
@@ -0,0 +1,45 @@
+# 입금 횟수가 5회가 될 때 잔고를 기준으로 1%의 이자가 잔고에 추가되도록 코드를 변경해보세요.
+
+import random
+
+class Account:
+  account_count = 0
+
+  def __init__(self, name, balance):
+    self.name = name
+    self.balance = balance
+    self.bank = "SC은행"
+    self.deposit_count = 0
+
+    num1 = random.randint(0, 999)
+    num2 = random.randint(0, 99)
+    num3 = random.randint(0, 999999)
+
+    num1 = str(num1).zfill(3)  
+    num2 = str(num2).zfill(2)   
+    num3 = str(num3).zfill(6)   
+    self.account_number = num1 + '-' + num2 + '-' + num3
+
+    Account.account_count += 1
+
+  @classmethod
+  def get_account_num(cls):
+    print(cls.account_count)
+
+  def deposit(self, amount):
+    if amount >= 1:
+      self.balance += amount
+
+    self.deposit_count += 1
+    if self.deposit_count % 5 == 0:
+      self.balance = (self.balance * 1.01)
+
+  def withdraw(self, amount):
+    if self.balance > amount:
+      self.balance -= amount
+
+  def display_info(self):
+    print("은행이름: " + self.bank)
+    print("예금주: " + self.name)
+    print("계좌번호: " +  self.account_number)
+    print("잔고: " + format(self.balance, ",") + "원")
